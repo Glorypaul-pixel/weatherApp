46e82cb98d31e0d83e9905ffe316ce32d0d0a5be
Glorypaul-pixel <ifeomaglory1998@gmail.com>
2023-07-22 21:44:57 +0100

    how is the weather over there

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-const apiKey = "";
-const apiUrl = "";
-// async function
+const apiKey = "aa4fec6c310df80f65e04e7256844f13";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?unit=metric";
+
+async function checkWeather() {
+  const response = await fetch(apiUrl + `&appid=${apiKey}`);
+  var data = await response.json();
+
+  console.log(data);
+}
