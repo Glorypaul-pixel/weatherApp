42775b829390f5171210dd7b4a9915a66e43459d
Glorypaul-pixel <ifeomaglory1998@gmail.com>
2023-08-01 09:19:59 +0100

    i am done

--- script.js
+++ script.js
@@ -5,23 +5,25 @@ const searchBox = document.querySelector("#search-bar input");
 const weatherImg = document.querySelector(".weather-img");
 const searchBar = document.querySelector("#search-icon");
 const description = document.querySelector(".description");
+const weatherBody = document.querySelector(".weather");
 const locNotFnd = document.querySelector(".location-not-found");
 
 async function checkWeather(city) {
   const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
   var data = await response.json();
-
+  if (data.cod === "404") {
+    locNotFnd.style.display = "flex";
+    weatherBody.style.display = "none";
+    console.log("error");
+    return;
+  }
   console.log(data);
   document.querySelector(".city").innerHTML = data.name;
   document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
   document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
   description.innerHTML = "<em>" + `${data.weather[0].description}` + "</em>";
   document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
-  if (response.cod === "404") {
-    locNotFnd.style.display = "flex";
-    console.log("error");
-    return;
-  }
+
   switch (data.weather[0].main) {
     case `Clouds`:
       weatherImg.src = `./img/scattered.png`;
@@ -29,13 +31,13 @@ async function checkWeather(city) {
     case `moderate`:
       weatherImg.src = `img/transparent cloud.png`;
     case `Clear`:
-      weatherImg.src = `./img/sunn.png`;
+      weatherImg.src = `./img/sun.png`;
       break;
     case `Snow`:
       weatherImg.src = `./img/snow.png`;
       break;
     case `Rain`:
-      weatherImg.src = `./img/snow.png`;
+      weatherImg.src = `./img/Rain.png`;
       break;
     case `Mist`:
       weatherImg.src = `./img/mist.jpeg`;
